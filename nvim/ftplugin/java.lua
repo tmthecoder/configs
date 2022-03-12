6962b2100d6bae7e14b9fe21bb83bc92dc237ca6
Tejas Mehta <tmthecoder@gmail.com>
2022-03-12 15:15:33 -0500

    successfully setup nvim-jdtls

---
+++ nvim/ftplugin/java.lua
@@ -0,0 +1,70 @@
+-- If you started neovim within `~/dev/xy/project-1` this would resolve to `project-1`
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+
+local workspace_dir = vim.fn.expand('~') .. '/Library/Java/jdtls-cache/' .. project_name
+
+local jdtls_dir = vim.fn.expand('~') .. '/Library/Java/jdtls-1.9.0'
+
+local config = {
+  -- The command that starts the language server
+  -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
+  cmd = {
+
+    'java', -- or '/path/to/java11_or_newer/bin/java'
+            -- depends on if `java` is in your $PATH env variable and if it points to the right version.
+
+    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+    '-Dosgi.bundles.defaultStartLevel=4',
+    '-Declipse.product=org.eclipse.jdt.ls.core.product',
+    '-Dlog.protocol=true',
+    '-Dlog.level=ALL',
+    '-Xms1g',
+    '--add-modules=ALL-SYSTEM',
+    '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+    '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+
+    '-jar', jdtls_dir .. '/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar',
+         -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                       ^^^^^^^^^^^^^^
+         -- Must point to the                                                     Change this to
+         -- eclipse.jdt.ls installation                                           the actual version
+
+
+    '-configuration', jdtls_dir .. '/config_mac',
+                    -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        ^^^^^^
+                    -- Must point to the                      Change to one of `linux`, `win` or `mac`
+                    -- eclipse.jdt.ls installation            Depending on your system.
+
+
+    -- See `data directory configuration` section in the README
+    '-data', workspace_dir
+  },
+
+  -- This is the default if not provided, you can remove it. Or adjust as needed.
+  -- One dedicated LSP server & client will be started per unique root_dir
+  root_dir = require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew'}),
+
+  -- Here you can configure eclipse.jdt.ls specific settings
+  -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+  -- for a list of options
+  settings = {
+    java = {
+    }
+  },
+}
+
+
+config.on_attach = function(client, bufnr)
+     require('jdtls').setup_dap({ hotcodereplace = 'auto' })
+     require('jdtls').add_commands()
+   end
+
+require('jdtls').start_or_attach(config)
+
+vim.cmd[[
+  command! -buffer JdtCompile lua require('jdtls').compile()
+  command! -buffer JdtUpdateConfig lua require('jdtls').update_project_config()
+  command! -buffer JdtJol lua require('jdtls').jol()
+  command! -buffer JdtBytecode lua require('jdtls').javap()
+  command! -buffer JdtJshell lua require('jdtls').jshell()
+  command! -buffer JdtSetupDapMainClassConfig lua require('jdtls.dap').setup_dap_main_class_configs()
+]]
