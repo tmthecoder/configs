0d338ab427735c40a91208dd3d540752850757b0
Tejas Mehta <tmthecoder@gmail.com>
2022-10-26 11:43:58 -0400

    Add typescript tsx & react filetypes

--- nvim/lua/plugins/typescript.lua
+++ nvim/lua/plugins/typescript.lua
@@ -2,6 +2,7 @@ local on_attach = require("../generic/lsp-on-attach")
 
 require('typescript').setup({
     server = {
+        filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
         on_attach = on_attach
     }
 })
