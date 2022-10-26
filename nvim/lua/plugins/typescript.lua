42aa3373cce21eb77797ad994f9a93b93eb1df07
Tejas Mehta <tmthecoder@gmail.com>
2022-10-26 11:54:53 -0400

    Revert "Add typescript tsx & react filetypes"
    This reverts commit 0d338ab427735c40a91208dd3d540752850757b0.

--- nvim/lua/plugins/typescript.lua
+++ nvim/lua/plugins/typescript.lua
@@ -2,7 +2,6 @@ local on_attach = require("../generic/lsp-on-attach")
 
 require('typescript').setup({
     server = {
-        filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
         on_attach = on_attach
     }
 })
