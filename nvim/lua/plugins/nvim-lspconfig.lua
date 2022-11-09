50d3e98ba6dccf1351c91a84eab67bbde93e41ba
Tejas Mehta <tmthecoder@gmail.com>
2022-11-08 19:24:18 -0500

    Update deprecated calls

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -14,8 +14,7 @@ local servers = {
     "jsonls"
 }
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 local diagnisticls_opts = {
     on_attach = on_attach,
--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,22 +1,22 @@
 local nvim_tree = require('nvim-tree')
 nvim_tree.setup {
-    open_on_tab = true,
-    open_on_setup = true,
-    hijack_directories = {
-        enable = true,
-        auto_open = true
-    }
+  open_on_tab = true,
+  open_on_setup = true,
+  hijack_directories = {
+    enable = true,
+    auto_open = true
+  }
 }
 
 local nvim_tree_events = require('nvim-tree.events')
-local bufferline_state = require('bufferline.state')
+local bufferline_api = require('bufferline.api')
 
-nvim_tree_events.on_tree_open(function ()
-  bufferline_state.set_offset(31, "File Tree")
+nvim_tree_events.on_tree_open(function()
+  bufferline_api.set_offset(31, "File Tree")
 end)
 
-nvim_tree_events.on_tree_close(function ()
-  bufferline_state.set_offset(0)
+nvim_tree_events.on_tree_close(function()
+  bufferline_api.set_offset(0)
 end)
 
 vim.api.nvim_create_autocmd("BufEnter", {
@@ -29,7 +29,7 @@ vim.api.nvim_create_autocmd("BufEnter", {
 })
 
 vim.api.nvim_create_user_command('NvimTreeToggle', function()
-    nvim_tree.toggle()
-  end,
+  nvim_tree.toggle()
+end,
   {}
 )
