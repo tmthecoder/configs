c38546e48fe2e06905c154b13da281a065baad2f
Tejas Mehta <tmthecoder@gmail.com>
2023-09-05 12:22:13 -0400

    Fix autoformat

--- nvim/lua/generic/lsp-on-attach.lua
+++ nvim/lua/generic/lsp-on-attach.lua
@@ -27,7 +27,7 @@ local on_attach = function(client, buffnr)
         vim.api.nvim_create_autocmd("BufWritePre", {
             group = vim.api.nvim_create_augroup("Format", { clear = true }),
             buffer = buffnr,
-            callback = function() vim.lsp.buf.formatting_seq_sync() end
+            callback = function() vim.lsp.buf.format() end
 
         })
     end
